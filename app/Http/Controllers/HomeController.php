3103177efdc87528887e72b7673f566cb3d997c3
Valdy Putra <vivaaldy@gmail.com>
2023-02-28 09:02:07 +0700

    bersihkan code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,6 @@ session_start();
 class HomeController extends Controller
 {
 	use SessionCheckTraits;
-	// use SessionCheckNotif;
 
 	private $db;
 
@@ -42,24 +41,6 @@ class HomeController extends Controller
 
 	public function display_menus($query, $parent, $level = 0, $idgroup)
 	{
-		// if ($parent == 0) {
-		// 	$sao = "(sao = 0 or sao is null)";
-		// } else {
-		// 	$sao = "(sao = ".$parent.")";
-		// }
-							
-		// $query = DB::select( DB::raw("
-		// 			SELECT *
-		// 			FROM ".$this->db.".dbo.sec_menu
-		// 			JOIN ".$this->db.".dbo.sec_access ON ".$this->db.".dbo.sec_access.idtop = ".$this->db.".dbo.sec_menu.ids
-		// 			WHERE ".$this->db.".dbo.sec_access.idgroup = '$idgroup'
-		// 			AND ".$this->db.".dbo.sec_access.zviw = 'y'
-		// 			AND $sao
-		// 			AND ".$this->db.".dbo.sec_menu.tampilnew = 1
-		// 			ORDER BY ".$this->db.".dbo.sec_menu.urut
-		// 			"));
-		// $query = json_decode(json_encode($query), true);
-
         $query = DB::connection('sqlsrv')->table('bpadkobar.dbo.sec_menu AS menu')->select()
                 ->join('bpadkobar.dbo.sec_access AS access', 'access.idtop', '=', 'menu.ids')
                 ->where('access.idgroup', $idgroup)
@@ -156,7 +137,6 @@ class HomeController extends Controller
 		$this->checkSessionTime();
 		
 		unset($_SESSION['user_kobar']);
-		// unset($_SESSION['notifs_kobar']);
 
 		date_default_timezone_set('Asia/Jakarta');
 
@@ -173,11 +153,6 @@ class HomeController extends Controller
 			]);	
 		} else {
 			$iduser = Auth::user()->id_emp;
-
-			// $client = new \GuzzleHttp\Client();
-			// $respuser = $client->request('GET', 'https://bpad.jakarta.go.id/portal/ws/getuserdata?user='.$iduser);
-   //      	$user_data = json_decode($respuser->getBody())->pegawai;
-   //      	$user_data = json_decode(json_encode($user_data), true);
 
 			$user_data = DB::select( DB::raw("
 						SELECT id_emp, nrk_emp, nip_emp, nm_emp, a.idgroup_aset as idgroup, tgl_lahir, jnkel_emp, tgl_join, status_emp, tbjab.idjab, tbjab.idunit, tbunit.child, tbunit.nm_unit from bpadkobar.dbo.emp_data as a
@@ -199,45 +174,6 @@ class HomeController extends Controller
 					"))[0];
 		$homeview = json_decode(json_encode($homeview), true);
 
-		// $notifs = $this->checknotif(Auth::user()->usname);
-		
-		// if (is_null(Auth::user()->usname)) {
-		// 	$iduser = Auth::user()->id_emp;
-
-		// 	$notifs = $this->checknotif(Auth::user()->id_emp);
-
-		// 	$user_data = DB::select( DB::raw("
-		// 				SELECT id_emp,a.uname+'::'+convert(varchar,a.tgl)+'::'+a.ip,createdate,nip_emp,nrk_emp,nm_emp,nrk_emp+'-'+nm_emp as c2,gelar_dpn,gelar_blk,jnkel_emp,tempat_lahir,tgl_lahir,CONVERT(VARCHAR(10), tgl_lahir, 103) AS [DD/MM/YYYY],idagama,alamat_emp,tlp_emp,email_emp,status_emp,ked_emp,status_nikah,gol_darah,nm_bank,cb_bank,an_bank,nr_bank,no_taspen,npwp,no_askes,no_jamsos,tgl_join,CONVERT(VARCHAR(10), tgl_join, 103) AS [DD/MM/YYYY],tgl_end,reason,a.idgroup,pass_emp,foto,ttd,a.telegram_id,a.lastlogin,tbgol.tmt_gol,CONVERT(VARCHAR(10), tbgol.tmt_gol, 103) AS [DD/MM/YYYY],tbgol.tmt_sk_gol,CONVERT(VARCHAR(10), tbgol.tmt_sk_gol, 103) AS [DD/MM/YYYY],tbgol.no_sk_gol,tbgol.idgol,tbgol.jns_kp,tbgol.mk_thn,tbgol.mk_bln,tbgol.gambar,tbgol.nm_pangkat,tbjab.tmt_jab,CONVERT(VARCHAR(10), tbjab.tmt_jab, 103) AS [DD/MM/YYYY],tbjab.idskpd,tbjab.idunit,tbjab.idjab, tbunit.child, tbjab.idlok,tbjab.tmt_sk_jab,CONVERT(VARCHAR(10), tbjab.tmt_sk_jab, 103) AS [DD/MM/YYYY],tbjab.no_sk_jab,tbjab.jns_jab,tbjab.idjab,tbjab.eselon,tbjab.gambar,tbdik.iddik,tbdik.prog_sek,tbdik.no_sek,tbdik.th_sek,tbdik.nm_sek,tbdik.gelar_dpn_sek,tbdik.gelar_blk_sek,tbdik.ijz_cpns,tbdik.gambar,tbdik.nm_dik,b.nm_skpd,c.nm_unit,c.notes,d.nm_lok, tbunit.sao FROM bpadkobar.dbo.emp_data as a
-		// 					CROSS APPLY (SELECT TOP 1 tmt_gol,tmt_sk_gol,no_sk_gol,idgol,jns_kp,mk_thn,mk_bln,gambar,nm_pangkat FROM  bpadkobar.dbo.emp_gol,bpadkobar.dbo.glo_org_golongan WHERE a.id_emp = emp_gol.noid AND emp_gol.idgol=glo_org_golongan.gol AND emp_gol.sts='1' AND glo_org_golongan.sts='1' ORDER BY tmt_gol DESC) tbgol
-		// 					CROSS APPLY (SELECT TOP 1 tmt_jab,idskpd,idunit,idlok,tmt_sk_jab,no_sk_jab,jns_jab,replace(idjab,'NA::','') as idjab,eselon,gambar FROM  bpadkobar.dbo.emp_jab WHERE a.id_emp=emp_jab.noid AND emp_jab.sts='1' ORDER BY tmt_jab DESC) tbjab
-		// 					CROSS APPLY (SELECT TOP 1 iddik,prog_sek,no_sek,th_sek,nm_sek,gelar_dpn_sek,gelar_blk_sek,ijz_cpns,gambar,nm_dik FROM  bpadkobar.dbo.emp_dik,bpadkobar.dbo.glo_dik WHERE a.id_emp = emp_dik.noid AND emp_dik.iddik=glo_dik.dik AND emp_dik.sts='1' AND glo_dik.sts='1' ORDER BY th_sek DESC) tbdik
-		// 					CROSS APPLY (SELECT TOP 1 * FROM bpadkobar.dbo.glo_org_unitkerja WHERE glo_org_unitkerja.kd_unit = tbjab.idunit) tbunit
-		// 					,bpadkobar.dbo.glo_skpd as b,bpadkobar.dbo.glo_org_unitkerja as c,bpadkobar.dbo.glo_org_lokasi as d WHERE tbjab.idskpd=b.skpd AND tbjab.idskpd+'::'+tbjab.idunit=c.kd_skpd+'::'+c.kd_unit AND tbjab.idskpd+'::'+tbjab.idlok=d.kd_skpd+'::'+d.kd_lok AND a.sts='1' AND b.sts='1' AND c.sts='1' AND d.sts='1' 
-		// 					and id_emp like '". Auth::user()->id_emp ."'
-		// 					"))[0];
-
-		// 	$user_data = json_decode(json_encode($user_data), true);
-
-		// 	Emp_data::where('id_emp', $user_data['id_emp'])
-		// 	->update([
-		// 		'lastlogin' => date('Y-m-d H:i:s'),
-		// 	]);	
-		// } else {
-		// 	$iduser = Auth::user()->usname;
-
-		// 	$user_data = Sec_logins::
-		// 					where('usname', $iduser)
-		// 					->first();
-
-		// 	Sec_logins::where('usname', $user_data['usname'])
-		// 	->update([
-		// 		'lastlogin' => date('Y-m-d H:i:s'),
-		// 	]);	
-
-		// 	$notifs = $this->checknotif(Auth::user()->usname);
-		// }
-
-		// $_SESSION['kobar_notifs'] = $notifs;
 		$_SESSION['kobar_data'] = $user_data;
 
 		$all_menu = [];
