46889cd7ca1cdabbd85973720a0df88a9fd2eaf0
Valdy Putra <vivaaldy@gmail.com>
2023-11-27 10:35:42 +0700

    ubah server76 ke server12

--- app/Nabar.php
+++ app/Nabar.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nabar extends Model
 {
-    protected $connection = 'server76';
+    protected $connection = 'server12';
 	// protected $primaryKey = "id_emp"; 
 	protected $table = "bpadkobar.dbo.data_nabar";
 	
